4271a0a7bdc5bc378296bf0e878fbf51a355bb7f
Anna <annaersh1994@gmail.com>
2020-03-22 02:50:35 +0300

    feat: add Dates-task

--- RSSchool_T2/RSSchool_T2/Exercises/1/Dates.m
+++ RSSchool_T2/RSSchool_T2/Exercises/1/Dates.m
@@ -3,7 +3,30 @@
 @implementation Dates
 
 - (NSString *)textForDay:(NSString *)day month:(NSString *)month year:(NSString *)year {
-    return @"";
+    
+    NSDateComponents *components = [[NSDateComponents new] autorelease];
+    [components setDay:day.intValue];
+    [components setMonth:month.intValue];
+    [components setYear:year.intValue];
+    NSCalendar *cal = [NSCalendar currentCalendar];
+    components.calendar = cal;
+    BOOL isValid = components.isValidDate;
+    if (!isValid)
+    {
+       return @"Такого дня не существует";
+    }
+    
+    NSDate *date = [cal dateFromComponents:components];
+
+    NSDateFormatter *dateFormatter = [[NSDateFormatter new] autorelease];
+    [dateFormatter setDateFormat:@"dd MMMM, EEEE"];
+    dateFormatter.locale = [NSLocale localeWithLocaleIdentifier:@"ru_RU"];
+    
+    NSString *weekday = [dateFormatter stringFromDate:date];
+    NSLog(@"%@", weekday);
+  
+    return weekday;
 }
 
 @end
+
