e16b4c579511bc4779d39c12cada8be0ef04a871
Anna <annaersh1994@gmail.com>
2020-03-22 02:47:26 +0300

    init: start with blocks

--- RSSchool_T2/RSSchool_T2/Exercises/5/Blocks.m
+++ RSSchool_T2/RSSchool_T2/Exercises/5/Blocks.m
@@ -2,6 +2,34 @@
 
 @implementation Blocks
 
+- (BlockC)blockC {
+    _blockC = ^(NSObject* result) {
+        result = @"43";
+        NSLog(@"get values to blockC");
+    };
+    return _blockC;
+}
+-(BlockB)blockB {
+    _blockB = ^(Class class) {
+        NSLog(@"get class to blockB");
+    };
+    return _blockB;
+}
+-(BlockA)blockA {
+    _blockA = ^(NSArray* array) {
+        NSLog(@"get array to blockA");
+    };
+    return _blockA;
+}
+
+- (void)dealloc {
+    NSLog(@"%@ IS DEALLOCATING", self);
+    
+    [_blockA release];
+    [_blockB release];
+    [_blockC release];
+    [super dealloc];
+}
 
 @end
 
