6fdf3c0cfe456d2fb9d21a78682ae35f90f5312e
Anna <annaersh1994@gmail.com>
2020-03-22 14:12:46 +0300

    feat: add Abbreviation-task

--- RSSchool_T2/RSSchool_T2/Exercises/3/AbbreviationMaker.h
+++ RSSchool_T2/RSSchool_T2/Exercises/3/AbbreviationMaker.h
@@ -1,5 +1,10 @@
 #import <Foundation/Foundation.h>
 
 @interface AbbreviationMaker : NSObject
+
+@property (nonatomic, retain) NSMutableString* str;
+@property (nonatomic, retain) NSCountedSet* aset;
+@property (nonatomic, retain) NSCountedSet* bset;
+
 - (NSString *) abbreviationFromA:(NSString *)a toB:(NSString *)b;
 @end
--- RSSchool_T2/RSSchool_T2/Exercises/3/AbbreviationMaker.m
+++ RSSchool_T2/RSSchool_T2/Exercises/3/AbbreviationMaker.m
@@ -1,8 +1,75 @@
 #import "AbbreviationMaker.h"
 
-@implementation AbbreviationMaker
-// Complete the abbreviation function below.
-- (NSString *) abbreviationFromA:(NSString *)a toB:(NSString *)b {
-    return @"Test";
+@interface NSArray(Array)
++ (NSArray*)splitStringIntoArray:(NSString*)string;
+@end
+
+@implementation NSArray(Array)
++ (NSArray*)splitStringIntoArray:(NSString*)string;
+{
+    NSMutableArray *array = [NSMutableArray array];
+    for (int i = 0; i < string.length; i++) {
+        NSString *ch = [string substringWithRange:NSMakeRange(i, 1)];
+        [array addObject:ch];
+    }
+    return array;
 }
+@end
+
+@implementation AbbreviationMaker
+
+-(void)dealloc
+{
+    [_str release];
+    [super dealloc];
+}
+- (NSString *) abbreviationFromA:(NSString *)a toB:(NSString *)b {
+    if (!_str)
+    {
+        _str = [a.uppercaseString mutableCopy];
+    }
+    
+    if ([self.str containsString:b]) {
+        return @"YES";
+    } else {
+        
+        NSMutableArray *arrayA = [NSArray splitStringIntoArray:a.lowercaseString].mutableCopy;
+        NSArray *arrayB = [NSArray splitStringIntoArray:b.lowercaseString];
+        NSMutableArray *arr = [NSArray splitStringIntoArray:a.uppercaseString].mutableCopy;
+        
+        NSMutableOrderedSet *orderedSet= [[NSMutableOrderedSet new] autorelease];
+        
+        
+        NSSet *set2 = [NSSet setWithArray:arrayA];
+        NSSet *set3 = [NSSet setWithArray:[NSArray splitStringIntoArray:b.uppercaseString]];
+        NSCountedSet *ss = [[[NSCountedSet alloc]initWithSet:set2] autorelease];
+        for (NSString *elementB in arrayB) {
+            for (NSString *elementA in arrayA) {
+                if ([elementB isEqual: elementA]) {
+                    [ss removeObject:elementA];
+                    [ss addObject:elementA.uppercaseString];
+                    NSRange range = [_str rangeOfString:elementA.uppercaseString];
+                    [arr removeObjectsInRange: NSMakeRange(0, range.location)];
+                    [orderedSet addObjectsFromArray:arr.copy];
+                    if (![orderedSet containsObject:elementA.uppercaseString])
+                    {
+                        return @"NO";
+                    }
+                    break;
+                } else {
+                    [ss removeObject:elementA];
+                }
+            }
+        }
+        
+        if ([set3 isSubsetOfSet:ss])
+        {
+            return @"YES";
+        }
+    }
+    
+    [self dealloc];
+    return @"NO";
+}
+
 @end
