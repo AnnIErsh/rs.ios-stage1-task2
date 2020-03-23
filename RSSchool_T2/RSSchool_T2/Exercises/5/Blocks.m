f2bf2f16660a75321609cd3664182fe7ced3198c
Anna <annaersh1994@gmail.com>
2020-03-23 17:27:59 +0300

    feat: add blocks-task

--- RSSchool_T2/RSSchool_T2/Exercises/5/Blocks.m
+++ RSSchool_T2/RSSchool_T2/Exercises/5/Blocks.m
@@ -1,30 +1,59 @@
 #import "Blocks.h"
 
 @implementation Blocks
-
-- (BlockC)blockC {
-    _blockC = ^(NSObject* result) {
-        result = @"43";
-        NSLog(@"get values to blockC");
-    };
-    return _blockC;
+ 
+- (NSArray *)arr{
+    return _arr;
 }
+
++ (NSArray*)ckeckElementIn:(NSArray*)myArray withClass:(Class)myClass {
+    NSMutableArray *elements = @[].mutableCopy;
+    for (id object in myArray) {
+        if ([object isKindOfClass:[myClass class]]) {
+            [elements addObject:object];
+        }
+    }
+    return elements.copy;
+}
+
 -(BlockB)blockB {
     _blockB = ^(Class class) {
-        NSLog(@"get class to blockB");
+        _arr = [Blocks ckeckElementIn: _arr withClass:class];
+        
+        if (class == [NSNumber class]) {
+            self.blockC([self.arr valueForKeyPath:@"@sum.self"]);
+            NSLog(@"get sum");
+        }
+        
+        if (class == [NSString class])
+        {
+            self.blockC([self.arr componentsJoinedByString:@""]);
+            NSLog(@"concat");
+        }
+        
+        if (class == [NSDate class])
+        {
+            [_arr sortedArrayUsingSelector:@selector(compare:)];
+            NSDateFormatter *formatter = [[NSDateFormatter new] autorelease];
+            [formatter setDateFormat:@"dd.MM.yyyy"];
+            self.blockC([formatter stringFromDate: _arr.firstObject]);
+            NSLog(@"date");
+        }
+        NSLog(@"get class");
     };
-    return _blockB;
+    return [_blockB copy];
 }
+
 -(BlockA)blockA {
     _blockA = ^(NSArray* array) {
+        _arr = array;
         NSLog(@"get array to blockA");
     };
-    return _blockA;
+    return [_blockA copy];
 }
 
 - (void)dealloc {
     NSLog(@"%@ IS DEALLOCATING", self);
-    
     [_blockA release];
     [_blockB release];
     [_blockC release];
