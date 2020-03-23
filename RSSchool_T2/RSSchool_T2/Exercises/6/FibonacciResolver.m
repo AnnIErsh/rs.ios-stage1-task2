94750d57f5e3749eb9e917a11e217dcc73a27c93
Anna <annaersh1994@gmail.com>
2020-03-23 15:30:25 +0300

    feat: add fibonacci

--- RSSchool_T2/RSSchool_T2/Exercises/6/FibonacciResolver.m
+++ RSSchool_T2/RSSchool_T2/Exercises/6/FibonacciResolver.m
@@ -2,6 +2,33 @@
 
 @implementation FibonacciResolver
 - (NSArray*)productFibonacciSequenceFor:(NSNumber*)number {
-    return @[];
+
+    NSNumber *first = @0;
+    NSNumber *second = @1;
+    
+    int a = first.intValue;
+    int b = second.intValue;
+    int prod = 0;
+    
+    for (; number.intValue >= prod;)
+    {
+        prod = a * b;
+        if (number.intValue == prod)
+            return @[@(a), @(b), @1];
+        
+        int sum = a + b;
+        a = b;
+        b = sum;
+    }
+    [self dealloc];
+    return @[@(b - a), @(a), @0];
+}
+
+
+- (void)dealloc
+{
+    [super dealloc];
 }
 @end
+
+
